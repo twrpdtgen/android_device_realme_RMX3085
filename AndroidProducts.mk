988e35b7b23f2c5e1b2553a99475bf359c1043f7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-10-28 10:37:50 +0200

    RMX3085: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: realme Model: RMX3085 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_RMX3085.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_RMX3085-user \
+    omni_RMX3085-userdebug \
+    omni_RMX3085-eng
---
+++ omni_RMX3085.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from RMX3085 device
+$(call inherit-product, device/realme/RMX3085/device.mk)
+
+PRODUCT_DEVICE := RMX3085
+PRODUCT_NAME := omni_RMX3085
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3085
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="twrp_RMX3085-eng 16.1.0 SP2A.220405.004 eng.sk.20231028.095332 test-keys"
+
+BUILD_FINGERPRINT := realme/twrp_RMX3085/RMX3085:16.1.0/SP2A.220405.004/sk10280950:eng/test-keys
